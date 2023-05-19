640a2526b5c4ac0996d77bf917611cb096fa7eea
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-19 15:02:42 -0500

    add new/create bike action

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -3,4 +3,20 @@ class BikesController < ApplicationController
     @bikes = Bike.all
     render :index
   end
+
+  def new
+    @bike = Bike.new
+    render :new
+  end
+
+  def create
+    @bike = Bike.create(
+      name: parmas[:bike][:name],
+      color: params[:bike][:color],
+      price: params[bike][:price],
+    )
+    redirect_to "/bikes"
+  end
+  
+  
 end
